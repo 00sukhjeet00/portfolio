c37fec23891543df34f6134d81aa6b67334f3b85
00sukhjeet00 <sukhjeet20002@gmail.com>
2023-03-03 14:32:00 +0530

    skill

--- src/lib/component/Skill.tsx
+++ src/lib/component/Skill.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import Title from './Title'
-
+import React from "react";
+import Title from "./Title";
+import { Skills } from "@/lib/utils/Skill";
 export default function Skill() {
   return (
-    <div className='pt-20'>
-        <Title title={"Skills"} num={"01"}/>
+    <div className="pt-20">
+      <Title title={"Skills"} num={"01"} />
+      <div className="center flex justify-evenly">
+        {Skills.map((item) => (
+          <a href={item.link} target="_blank">
+            <img
+              src={item.img}
+              alt="React & React Native"
+              width="60"
+              height="60"
+              style={{filter:item.name=="Next Js"? "invert(1)":"none"}}
+            />
+            <p className="text-white">{item.name}</p>
+          </a>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/lib/utils/Skill.js
@@ -0,0 +1,48 @@
+export const Skills = [
+  {
+    name: "React Js",
+    img: "https://img.icons8.com/plasticine/100/000000/react.png",
+    link: "https://reactjs.org/",
+  },
+  {
+    link: "https://nodejs.org/",
+    img: "https://img.icons8.com/color/48/000000/nodejs.png",
+    name: "Node Js",
+  },
+  {
+    link: "https://www.mongodb.com/",
+    img: "https://img.icons8.com/color/48/000000/mongodb.png",
+
+    name: "MongoDB",
+  },
+  {
+    link: "https://www.postman.com/",
+    img: "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/postman-icon.png",
+
+    name: "Postman",
+  },
+  {
+    link: "https://firebase.google.com/",
+    img: "https://img.icons8.com/color/50/000000/google-firebase-console.png",
+
+    name: "Firebase",
+  },
+  {
+    link: "https://redux.js.org/",
+    img: "https://img.icons8.com/color/48/000000/redux.png",
+
+    name: "Redux",
+  },
+  {
+    link: "https://www.docker.com/",
+    img: "https://img.icons8.com/color/48/000000/docker.png",
+
+    name: "Docker",
+  },
+  {
+    link: "https://nextjs.org//",
+    img: "https://static-00.iconduck.com/assets.00/next-js-icon-512x512-zuauazrk.png",
+
+    name: "Next Js",
+  },
+];
