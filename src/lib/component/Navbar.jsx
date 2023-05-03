67349ddf23932a70ab3c4c254cda1139754605db
00sukhjeet00 <sukhjeet20002@gmail.com>
2023-05-03 08:04:05 +0530

    link

--- src/lib/component/Navbar.jsx
+++ src/lib/component/Navbar.jsx
@@ -47,7 +47,7 @@ export default function Navbar() {
         </div> */}
         <a
           className="lg:hidden md:hidden relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
-          href="https://drive.google.com/file/d/1auqldDfy04YE0k603JDmz-UYPqkdyUHm/view?usp=share_link"
+          href="https://drive.google.com/file/d/1nFW5K3-Iw7jQVONSHfLa0cePTQOQeC0G/view?usp=sharing"
           target={"_blank"}
         >
           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
