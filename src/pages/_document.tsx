e1e64d7c19d941d7419f9c3deb873104c033eb1e
00sukhjeet00 <sukhjeet20002@gmail.com>
2023-10-03 10:17:15 +0530

    Build issue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
@@ -17,14 +16,7 @@ export default function Document() {
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-5BN5SCRK8N"
         ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {
-            // @ts-ignore
-            dataLayer.push(arguments)
-          }
-          gtag('js', new Date()); gtag('config', 'G-5BN5SCRK8N');
-        </script>
+        <script src="analytics.js"></script>
         <script src="silder.js"></script>
         <script src="mouse.js"></script>
         <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
